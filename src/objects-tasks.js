6eeefec73cf4a5d27e1914d67e3a78cda7486615
Egor Perfilyev <perfilev.egor2016@gmail.com>
2024-03-13 11:24:39 +0300

    feat: implement mergeObjects function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -34,8 +34,19 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const resObj = {};
+  objects.forEach((obj) => {
+    Object.entries(obj).forEach(([key, value]) => {
+      if (resObj[key]) {
+        resObj[key] += value;
+      } else {
+        resObj[key] = value;
+      }
+    });
+  });
+
+  return resObj;
 }
 
 /**
